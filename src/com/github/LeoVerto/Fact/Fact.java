456ca05aff212ece8d51661a95a0ec7d7f5e1b8b
Leo Verto <leotheverto@googlemail.com>
2012-08-20 22:49:12 +0200

    Added language config (closes 5) + reload command (closes 4)

--- src/com/github/LeoVerto/Fact/Fact.java
+++ src/com/github/LeoVerto/Fact/Fact.java
@@ -26,6 +26,7 @@ public class Fact extends JavaPlugin {
 
 	public void loadConfiguration() {
 		final String path = "Fact.";
+		reloadConfig();
 		getConfig().addDefault(path + "Colors.PlayerFact.Fact", "'&7'");
 		getConfig().addDefault(path + "Colors.PlayerFact.Text", "'&f'");
 		getConfig().addDefault(path + "Colors.ConsoleFact.Fact", "'&6'");
@@ -35,7 +36,11 @@ public class Fact extends JavaPlugin {
 		getConfig().addDefault(path + "Messages.AutoFact.Delay", 5);
 		getConfig().addDefault(path + "Messages.AutoFact.Facts",
 				Arrays.asList("This is a default fact.", "You can change autofacts in /plugins/Fact/config.yml", "Default stuff is usually bad, so please change this!"));
+		getConfig().addDefault(path + "Text.Fact", "Fact>");
+		getConfig().addDefault(path + "Text.AutoFact", "AutoFact>");
 		getConfig().options().copyDefaults(true);
+		//Currently no header
+		//getConfig().options().copyHeader(true);
 		saveConfig();
 	}
 
@@ -46,19 +51,19 @@ public class Fact extends JavaPlugin {
 		final int		messageCount	= messages.size();
 		final String	FactColor		= ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.AutoFact.Fact").replace("'", ""));
 		final String	TextColor		= ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.AutoFact.Text").replace("'", ""));
+		final String	AutoFactText	= getConfig().getString("Fact.Text.AutoFact");
 
-		getLogger().info("AutoFact Delay: " + Long.toString(autoFactDelay));
 		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 			private int	messageNumber	= 0;
 
 			@Override
 			public void run() {
 				if (messageNumber < (messageCount)) {
-					Bukkit.getServer().broadcastMessage(FactColor + "AutoFact> " + TextColor + messages.get(messageNumber));
+					Bukkit.getServer().broadcastMessage(FactColor + AutoFactText + " " + TextColor + messages.get(messageNumber));
 					messageNumber++;
 				} else {
 					messageNumber = 0;
-					Bukkit.getServer().broadcastMessage(FactColor + "AutoFact> " + TextColor + messages.get(messageNumber));
+					Bukkit.getServer().broadcastMessage(FactColor + AutoFactText + " " + TextColor + messages.get(messageNumber));
 					messageNumber++;
 				}
 			}
@@ -68,30 +73,48 @@ public class Fact extends JavaPlugin {
 	@Override
 	public boolean onCommand(final CommandSender sender, final Command cmd, final String commandLabel, final String[] args) {
 		if (cmd.getName().equalsIgnoreCase("fact")) {
-			if ((sender instanceof Player)) {
-				final Player player = (Player) sender;
-				if (player.hasPermission("fact.fact")) {
-					final String FactColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.PlayerFact.Fact").replace("'", ""));
-					final String TextColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.PlayerFact.Text").replace("'", ""));
+			if(args[0].equals("reload")) {
+				if ((sender instanceof Player)) {
+					final Player player = (Player) sender;
+					if (player.hasPermission("fact.reload")) {
+						loadConfiguration();
+						getLogger().info("Reload complete!");
+					} else {
+						player.sendMessage(this.getCommand("fact").getPermissionMessage());
+						return false;
+					}
+				} else {
+					loadConfiguration();
+					getLogger().info("Reload complete!");
+				}
+			} else {
+				if ((sender instanceof Player)) {
+					final Player player = (Player) sender;
+					if (player.hasPermission("fact.fact")) {
+						final String FactColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.PlayerFact.Fact").replace("'", ""));
+						final String TextColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.PlayerFact.Text").replace("'", ""));
+						final String FactText  = getConfig().getString("Fact.Text.Fact");
+						String message = "";
+						for (int i = 0; i < args.length; i++) {
+							message = (message + " " + args[i]);
+						}
+						Bukkit.getServer().broadcastMessage(FactColor + FactText + " " + TextColor + message);
+						return true;
+					} else {
+						player.sendMessage(this.getCommand("fact").getPermissionMessage());
+						return false;
+					}
+				} else {
+					final String FactColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.ConsoleFact.Fact").replace("'", ""));
+					final String TextColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.ConsoleFact.Text").replace("'", ""));
+					final String FactText  = getConfig().getString("Fact.Text.Fact");
 					String message = "";
 					for (int i = 0; i < args.length; i++) {
 						message = (message + " " + args[i]);
 					}
-					Bukkit.getServer().broadcastMessage(FactColor + "Fact> " + TextColor + message);
+					Bukkit.getServer().broadcastMessage(FactColor + FactText + " " + TextColor + message);
 					return true;
-				} else {
-					player.sendMessage(this.getCommand("fact").getPermissionMessage());
-					return false;
 				}
-			} else {
-				final String FactColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.ConsoleFact.Fact").replace("'", ""));
-				final String TextColor = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Fact.Colors.ConsoleFact.Text").replace("'", ""));
-				String message = "";
-				for (int i = 0; i < args.length; i++) {
-					message = (message + " " + args[i]);
-				}
-				Bukkit.getServer().broadcastMessage(FactColor + "Fact> " + TextColor + message);
-				return true;
 			}
 		}
 		return false;
